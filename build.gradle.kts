72e1fbfaaea60699d377b2907dddccf22ee6315e
Yesferal Cueva <yesferal@gmail.com>
2021-10-02 03:24:59 -0500

    Remove useless gradle code

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,15 +25,6 @@ kotlin {
 
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
     
     sourceSets {
         val commonMain by getting
@@ -42,12 +33,6 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
 
     val iosX64 = iosX64()
