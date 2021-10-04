5295c7676a82a16ff554c86dacb9b3f5c83f6f1d
Yesferal Cueva <yesferal@gmail.com>
2021-10-04 00:12:24 -0500

    Bump version to 0.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("maven-publish")
 }
 
-val libraryVersion = "0.3.0"
+val libraryVersion = "0.3.1"
 group = "com.yesferal.hornsapp.core"
 version = libraryVersion
 
---
+++ src/commonMain/kotlin/com/yesferal/hornsapp/core/HornsAppCoreVersion.kt
@@ -0,0 +1,5 @@
+package com.yesferal.hornsapp.core
+
+class HornsAppCoreVersion {
+    val version = "0.3.1"
+}
