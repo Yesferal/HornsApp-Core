e2c51577440b4833af43c969218566a7cebeed3b
Yesferal Cueva <yesferal@gmail.com>
2022-07-13 02:01:45 -0500

    [Doc] Add javadoc to Logger

--- src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/abstraction/Logger.kt
+++ src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/abstraction/Logger.kt
@@ -1,5 +1,13 @@
+/* Copyright © 2022 HornsApp. All rights reserved. */
 package com.yesferal.hornsapp.core.domain.abstraction
 
+/**
+ * This class is a Logger abstraction
+ * is part of a Chain Of Responsibility pattern
+ * and it should be implemented in the App UI Module
+ *
+ * @author Yesferal
+ */
 interface Logger {
     fun d(message: String)
     fun e(message: String)
