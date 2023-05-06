772f0df2d9ceefc9c183040ee07011a9e482d041
Yesferal Cueva <yesferal@gmail.com>
2023-05-06 18:44:37 -0500

    Add shortName as Venue Model param

--- src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/entity/Venue.kt
+++ src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/entity/Venue.kt
@@ -3,6 +3,7 @@ package com.yesferal.hornsapp.core.domain.entity
 data class Venue (
     val id: String,
     val name: String?,
+    val shortName: String?,
     val latitude: String?,
     val longitude: String?
 )
