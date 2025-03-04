77d1181684506744be4a4aafe9af793b7f7fbcf9
Yesferal Cueva <yesferal@gmail.com>
2025-03-04 18:36:35 -0500

    Remove unused NavigationDrawer class

--- src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/entity/drawer/NavigationDrawer.kt
+++
@@ -1,7 +0,0 @@
-/* Copyright © 2023 HornsApp. All rights reserved. */
-package com.yesferal.hornsapp.core.domain.entity.drawer
-
-class NavigationDrawer(
-    val key: String?,
-    val parameters: Map<String, String>?,
-)
