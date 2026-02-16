085c94118fffc7327831fe656bebf0a9af5754f4
Yesferal Cueva <yesferal@gmail.com>
2026-02-16 11:01:23 -0500

    Move home filter logic into Core Li

--- src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/entity/render/ChildrenRender.kt
+++ src/commonMain/kotlin/com/yesferal/hornsapp/core/domain/entity/render/ChildrenRender.kt
@@ -1,6 +1,8 @@
 /* Copyright © 2023 HornsApp. All rights reserved. */
 package com.yesferal.hornsapp.core.domain.entity.render
 
+import com.yesferal.hornsapp.core.domain.entity.Concert
+
 data class ChildrenRender(
     val key: String?,
     val filter: FilterRender?,
@@ -21,6 +23,62 @@ data class ChildrenRender(
             Type.UPCOMING_IMAGE_CARD_VIEW.name -> Type.UPCOMING_IMAGE_CARD_VIEW
             else -> Type.UNDETERMINED
         }
+
+
+    fun getConcertsForSections(
+        concerts: List<Concert>,
+    ): List<Concert> {
+        // Children On Demand
+        // FIXME: We can rename sort as onDemand or something similar
+        // due to we pick concerts on demand following the ViewRender model
+        val children = concerts
+            .filter { this.sort?.contains(it.id) == true }
+
+        return children.ifEmpty {
+            filterConcertsBySectionDataSource(
+                when (this.type) {
+                    Type.CAROUSEL_CARD_VIEW -> {
+                        concerts.reversed()
+                    }
+
+                    Type.UPCOMING_CARD_VIEW, Type.UPCOMING_IMAGE_CARD_VIEW -> {
+                        concerts.sortedWith(compareBy { it.timeInMillis })
+                    }
+
+                    else -> listOf()
+                }
+            )
+        }
+    }
+
+    private fun filterConcertsBySectionDataSource(
+        concerts: List<Concert>,
+    ): List<Concert> {
+        var events = concerts
+            .filter { concert ->
+                val events = this.filter?.events
+                if (events.isNullOrEmpty()) {
+                    true
+                } else {
+                    events.contains(concert.id)
+                }
+            }
+
+        if (events.isEmpty()) {
+            events = concerts
+        }
+
+        return events.filter { concert ->
+            val categories = this.filter?.categories
+            if (categories.isNullOrEmpty()) {
+                true
+            } else {
+                categories.any { anyCategory ->
+                    concert.categories?.contains(anyCategory) == true
+                }
+            }
+        }.take(this.take ?: Int.MAX_VALUE)
+    }
 }
 
 data class FilterRender(
